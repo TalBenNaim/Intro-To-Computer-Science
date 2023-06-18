d2ad50adbf148a8ed2830ffe3230208d325f2ad5
TalBenNaim <talbennaim13@gmail.com>
2023-06-18 12:18:35 +0300

    updare

--- ex_6/ex_6.c
+++ ex_6/ex_6.c
@@ -20,14 +20,23 @@ typedef struct FamilyHead {
     struct FamilyHead *next;
 } FamilyHead;
 
+// Defines
+#define NEW_LINE_ASCII 10
+
 // Funcs declarations
 
 // -> Helper funcs
 void printMenu();
 void cleanBuffer();
 int isChoiceValid(char option);
+void sendToMission(char choice, FamilyHead *firstHuman);
 void scanTree(FamilyHead *firstHuman);
-void scanTreeFurther(Human *human, Human dontScanAgain[], unsigned int index);
+void scanTreeFurther(Human *human, Human **dontScanAgain, unsigned int index);
+char *stringFromUser();
+int isPtrInArray(Human **arrayOfPtr, void *ptr, unsigned int arrayLength);
+
+// -> Missions funcs
+void createFamilyHead(FamilyHead *firstHuman);
 
 /**
  * @brief Main function that wait for user input to navigate the user
@@ -44,7 +53,6 @@ int main() {
     while (choice != '0') {
         // present the menu to the user and take his choice
         printMenu();
-
         scanf("%c", &choice);
         cleanBuffer();
 
@@ -53,26 +61,17 @@ int main() {
             printf("Invalid option\n");
         }
 
-        // depending on the user's choice navigate the user to the right program.
-        switch (choice) {
-            case '1':
-                break;
-            case '2':
-                break;
-            case '3':
-                break;
-            case '4':
-                break;
-            case '5':
-                break;
-            case '6':
-                break;
-            case '7':
-                break;
-
-            default:
-                break;
+        // create the firstHuman for the run.
+        FamilyHead *firstHuman = (FamilyHead *)malloc(sizeof(FamilyHead));
+        if (firstHuman == NULL) {
+            exit(1);
         }
+
+        // reset the value and next to null
+        firstHuman->properties = NULL;
+        firstHuman ->next = NULL;        
+
+        sendToMission(choice, firstHuman);
     }
 
     return 0;
@@ -125,7 +124,38 @@ int isChoiceValid(char choice) {
 }
 
 /**
+ * @brief this function gets a choice and moves the user to the right missions
+ * 
+ * @param choice the choice the user made
+ * @param firstHuman he is neede in all the missions
+*/
+void sendToMission(char choice, FamilyHead *firstHuman){
+    // depending on the user's choice navigate the user to the right program.
+    switch (choice) {
+        case '1':
+            createFamilyHead(firstHuman);
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
+        case '7':
+            break;
+
+        default:
+            break;
+    }
+}
+
+/**
  * @brief this function scans the tree, visiting every memeber without repetition
+ * if memory allocation fail we exit the program.
  * 
  * @param firstHuman the first family head in the whole tree.
  * 
@@ -140,9 +170,15 @@ void scanTree(FamilyHead *firstHuman){
         // send the next FH
         scanTree(firstHuman->next);
 
-        // create an array of humans to not scan again, empty for now
-        Human dontScanAgain[];
+        // create an array of humans to not scan again, empty for now. (allocate memory)
+        Human **dontScanAgain = (Human **)malloc(sizeof(Human));
+        if (dontScanAgain == NULL) {
+            exit(1);
+        }
+
+        // save the size of the array.
         static unsigned index = 0;
+
         // send his firstChild to the scanTreeFurther   
         scanTreeFurther(firstHuman->properties->firstChild, dontScanAgain, index);
 
@@ -156,16 +192,165 @@ void scanTree(FamilyHead *firstHuman){
  * @param human the human to scan
  * @param dontScanAgain an array to save the humans we don't want to scan again. (if we won't through them).
 */
-void scanTreeFurther(Human *human, Human dontScanAgain[], unsigned int index){
+void scanTreeFurther(Human *human, Human **dontScanAgain, unsigned int index){
     if(human != NULL){
+        // if he's in the dontScanAgain we return before proccessing
+        if(isPtrInArray(dontScanAgain, human, index) == 1){
+            return;
+        }
+
+        // do something, in this case printf just to check
+        printf("%s", human->name);
+
         // add him to the dontScanAgain array
+        dontScanAgain[index] = human;
+
+        /*
+        expand the memory according to the size thous far
+        we allocated index+1 to offset the diff of array numbering to actual numbers of memory blocks
+        */
+        dontScanAgain = (Human **)realloc(dontScanAgain, (index + 1) * sizeof(Human));
+        if (dontScanAgain == NULL) {
+            exit(1);
+        }
+
+        index++;
 
         // send this human firstChild to scanTreeFurther
-        scanTreeFurther(human->firstChild,dontScanAgain);
+        scanTreeFurther(human->firstChild,dontScanAgain, index);
 
         // send this human siblings to scanTreeFurther
-        scanTreeFurther(human->sibling, dontScanAgain);
+        scanTreeFurther(human->sibling, dontScanAgain, index);
+
+        return;
 
     }
 
+}
+
+/**
+ * @brief create a string from the user inputs.
+ * if memory allocation fail we exit the program.
+ *
+ * @return the string.
+ */
+char *stringFromUser() {
+    // initialize the str and allocate memory
+    char *str = (char *)malloc(sizeof(char));
+    if (str == NULL) {
+        exit(1);
+    }
+
+    unsigned int index = 0;
+    char usersInput;
+    do {
+        // take the user input into the current index of the str
+        scanf("%c", &str[index]);
+        usersInput = str[index];
+
+        // move the index
+        index++;
+
+        // expand the memory according to the size thous far
+        str = (char *)realloc(str, (index + 1) * sizeof(char));
+        if (str == NULL) {
+            exit(1);
+        }
+
+        // check the index we just inserted to and see if it's new line.
+    } while (usersInput != NEW_LINE_ASCII);
+
+    // return the pointer to the string we built
+    return str;
+}
+
+/**
+ * @brief check if the giving pointer is in the array of ptr
+ * 
+ * @param arrayOfPtr the array of pointers to check in
+ * @param ptr the pointer to look out for
+ * @param arrayLength the length of the array
+ * 
+ * @return 1 is in array, 0 isn't
+*/
+int isPtrInArray(Human **arrayOfPtr, void *ptr, unsigned int arrayLength){
+    // loop through each element of the array
+    for (unsigned int i = 0; i < arrayLength; i++) {
+        // if an element is equal to the ptr we are looing for, return 1;
+        if(arrayOfPtr[i] == ptr){
+            return 1;
+        }
+    }
+
+    // we didn't find it
+    return 0;
+}
+
+/**
+ * @brief get the last familyHead in the familyHead struct
+ *
+ * @param firstHuman the head of the familyHead struct
+ *
+ * @return the last node in the familyHead
+ */
+FamilyHead *lastFamilyHead(FamilyHead *firstHuman) {
+    // an helper pointer to store the head and not change it.
+    FamilyHead *helper = firstHuman;
+
+    // go next node until we see a null.
+    while (helper->next != NULL) {
+        // move the helper to be the next node
+        helper = helper->next;
+    }
+    // return the helper as the last node
+    return helper;
+}
+
+// -> Missions funcs
+
+/**
+ * @brief This function creates a family head with the name and age
+ * 
+ * @param firstHuman the pointer to the first human in the program
+*/
+void createFamilyHead(FamilyHead *firstHuman){
+    // get name from user
+    printf("Enter a name:\n");
+    char *name = stringFromUser();
+
+    // get age from user
+    printf("Enter age:\n");
+    unsigned int age;
+    scanf("%u",&age);
+    
+    // check if name doesn't exists with the scan upward (W.I.P)
+    int exists = 0; // just for testing
+
+    if(exists == 1){
+        printf("The name is already taken\n");
+    }
+
+    // allocate memory to human pointer
+    Human *newHumanPointer = (Human *)malloc(sizeof(Human));
+    if (newHumanPointer == NULL) {
+        exit(1);
+    }
+
+    // create the human with name and age, everything else is null.
+    Human human = {name, 0, NULL,NULL,NULL,NULL,NULL};
+
+    // take the human value in the pointer
+    newHumanPointer = &human;
+
+    // first familyHead? save the value to firstHuman
+    if(firstHuman->properties == NULL){
+        firstHuman->properties = newHumanPointer;
+        
+        // not the first? save in the last node of the familyHead
+    }else{
+        FamilyHead *lastPointer = lastFamilyHead(firstHuman);
+
+        // add the human to the last node of the familyHead.
+        lastPointer -> properties = newHumanPointer;
+    }
 }
